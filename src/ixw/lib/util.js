82a278a6e0b9899fcb3f613ab1c1ac2ff1feb6a8
yezhi <yezhi@netposa.com>
2016-07-04 16:00:44 +0800

    Signed-off-by: yezhi <yezhi@netposa.com>

---
+++ src/ixw/lib/util.js
@@ -0,0 +1,59 @@
+(function(){
+IX.ns("TNM.Util");
+
+var CommonQueryInterval = 5000; // 5 seconds
+TNM.Util.PeriodicChecker = function(condFn, checkFn, interval){
+	var isStarted = false;
+	var intv = interval || CommonQueryInterval;
+	var timers = null;
+	function _query(){
+		if (IX.isFn(condFn) && !condFn()){
+			isStarted = false;
+			return;
+		}
+		timers = setTimeout(function(){
+			if(isStarted) _query();
+		}, intv);
+		checkFn(function(data){ return false;});
+	}
+	return {
+		start  :function(){
+			if (!isStarted) _query();
+			isStarted = true;
+		},
+		stop : function(){
+			isStarted = false;
+			clearTimeout(timers);
+		}
+	};
+};
+
+TNM.Util.KickChecker = function(condFn, checkFn, interval){
+	var isStarted = false;
+	var intv = interval || CommonQueryInterval;
+	var timers = null;
+	function _query(){
+		if (IX.isFn(condFn) && !condFn()){
+			isStarted = false;
+			return;
+		}
+		timers = function(){
+			return setTimeout(function(){
+				if(isStarted) _query();
+			}, intv);
+		};
+		checkFn(timers);
+	}
+	return {
+		start : function(){
+			if (!isStarted) _query();
+			isStarted = true;
+		},
+		stop : function(){
+			isStarted = false;
+			clearTimeout(timers);
+		}
+	};
+};
+
+})();
